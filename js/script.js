b98003719a4e0bedee36b61d51c49ae83588765f
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2026-01-25 14:45:08 -0300

    deploy do site

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 let user = localStorage.getItem("nameUser");
 
 if (!user) {
-    user = prompt("Digite seu nome:");
-    if (user) localStorage.setItem("nameUser", user);
+  user = prompt("Digite seu nome:");
+
+  if (user) {
+    user = user.trim();
+    if (user.length > 0) {
+      localStorage.setItem("nameUser", user);
+    }
+  }
 }
 
-document.getElementById("user").innerText = user;
+if (user) {
+  let displayName = user.trim().split(" ")[0];
+
+  if (displayName.length < 2) {
+    displayName = user.substring(0, 10);
+  }
+
+  const userElement = document.getElementById("user");
+  if (userElement) {
+    userElement.innerText = displayName;
+  }
+}
